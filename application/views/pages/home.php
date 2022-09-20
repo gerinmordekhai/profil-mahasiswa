0ef2a00f7d505d91b574a60b4d84232165441b62
gerinmordekhai <mordekhaigerinlumangkun@gmail.com>
2022-09-20 12:03:15 +0700

    profil mahasiswa codeigniter3

---
+++ application/views/pages/home.php
@@ -0,0 +1,52 @@
+    <!--about me-->
+    <section id="about">
+        <div class="containercontent">
+        <div class="kolomimg">
+            <img src="<?php echo base_url('asset/img/fotoava.jpg'); ?>" class="fotoava" alt="Mordekhai Gerin" />
+        </div>
+        <div class="kolomcontent">
+            <h3>Mahasiswa,</h3>
+            <h1>Mordekhai Gerine Lumangkun</h1>
+            <h4 class="aboutme">
+            Perkenalkan nama saya Mordekhai Gerin, biasa di panggil Gerin. Saya berasal dari Kalimantan Timur tepatnya di Kabupaten Kutai Timur, Kota Sangatta. Saya suka editing, apalagi di masa pandemi hampir setiap hari saya melakukan
+            kegiatan ini. Saya pernah terlibat dalam projek pembuatan vidio dokumenter sekolah saya, dan juga projek vidio pelepasan angkatan saya. Selain itu, untuk mengisi waktu kosong saya suka bermain basket. Saya pernah mewakili sekolah
+            saya dalam lomba basket Honda DBL di provinsi saya. Selain itu juga, saya pernah mewakili sekolah saya dalam PASKIBRAKA di tingkat kabupaten tahun 2018.
+            </h4>
+            
+        </div>
+        </div>
+    </section>
+
+    <!--galery-->
+    <section id="galery">
+        <div class="containergalery">
+            <h1>Galery</h1>
+        </div>
+        <div class="containergalery">
+            <h3>Kumpulan Hasil Karya</h3>
+        </div>
+        <div class="galery">
+            <div class="kartu">
+                <div class="imgbox karya-1"></div>
+                <h3>Vector/Art</h3>
+            </div>
+            <div class="kartu">
+                <div class="imgbox karya-2"></div>
+                <h3>Vector/Art</h3>
+            </div>
+            <div class="kartu">
+                <div class="imgbox karya-3"></div>
+                <h3>Typography</h3>
+            </div>
+        </div>
+    </section>    
+
+    <!--contact-->
+    <section id="contact">
+        <div class="containercontact">
+            <h1>Contact</h1>
+        </div>
+        <div class="containercontact">
+            <h4>mordekhaigerinlumangkun@gmail.com</h4>
+        </div>
+    </section>
---
+++ application/views/templates/Header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!--css-->
+    <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url('asset/css/style.css'); ?>">
+    
+
+    <!--font-->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+
+    <title>Praktikum | Mordekhai Gerine</title>
+  </head>
+  <body>
+    <!--navbar-->
+    <nav>
+      <label class="portofolio">Profil Saya</label>
+    </nav>
